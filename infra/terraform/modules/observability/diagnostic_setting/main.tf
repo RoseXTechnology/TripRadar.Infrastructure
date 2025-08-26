91cee09339cc62fabe6cc712323b82d5bf6b480c
Anton Kharchenko <anton.kharchenko@pxpfinancial.com>
2025-08-26 14:34:40 +0300

    Fix: EH listen secret uses active slot local

--- infra/terraform/modules/observability/diagnostic_setting/main.tf
+++ infra/terraform/modules/observability/diagnostic_setting/main.tf
@@ -3,26 +3,25 @@ variable "target_resource_id" { type = string }
 variable "log_analytics_workspace_id" { type = string }
 
 # Discover categories dynamically for the target
-data "azurerm_monitor_diagnostic_categories" "this" {
-  resource_id = var.target_resource_id
-}
+# (Removed to avoid plan-time unknowns when target_resource_id is not yet known)
+# data "azurerm_monitor_diagnostic_categories" "this" {
+#   resource_id = var.target_resource_id
+# }
 
 resource "azurerm_monitor_diagnostic_setting" "this" {
   name                       = var.name
   target_resource_id         = var.target_resource_id
   log_analytics_workspace_id = var.log_analytics_workspace_id
 
-  dynamic "enabled_log" {
-    for_each = toset(data.azurerm_monitor_diagnostic_categories.this.log_category_types)
-    content {
-      category = enabled_log.value
-    }
+  # Enable all available log categories using category group
+  log {
+    category_group = "allLogs"
+    enabled        = true
   }
 
-  dynamic "enabled_metric" {
-    for_each = toset(data.azurerm_monitor_diagnostic_categories.this.metrics)
-    content {
-      category = enabled_metric.value
-    }
+  # Enable all available metrics
+  metric {
+    category = "AllMetrics"
+    enabled  = true
   }
 }
